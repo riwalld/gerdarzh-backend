d5e119c8358fdd5d52b158fd41fe263c75741b94
Benjamin D <57868057+riwalld@users.noreply.github.com>
2025-01-04 17:52:04 +0100

    fix (api) correcting propernoun api viewset

--- api_propernouns/views.py
+++ api_propernouns/views.py
@@ -1,7 +1,7 @@
 
 from rest_framework.response import Response
 from geriadur_api_django.models import Propernoun
-from rest_framework import viewsets
+from rest_framework import viewsets,status
 from .serializer import PropernounSerializer
 from rest_framework.decorators import action
 
@@ -15,7 +15,5 @@ class ProperNounsViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['get'], url_path='propernoun')
     def get_one(self, request):
         propernoun = Propernoun.objects.filter(current_name=request.query_params.get("current_name", None)).first()
-        data = {
-            "current_name": propernoun.current_name,
-        }
-        return Response(data, status=200)
+        serializer = self.get_serializer(propernoun)
+        return Response(serializer.data, status=status.HTTP_200_OK)
