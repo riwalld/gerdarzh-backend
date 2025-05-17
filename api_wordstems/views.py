9fed2a5de7cfb5d0a34dcf5cb86c17ecfcc20bae
Benjamin D <57868057+riwalld@users.noreply.github.com>
2025-05-17 07:59:41 +0200

    feat (serializers) add related wordstems (parents and children)

--- api_wordstems/views.py
+++ api_wordstems/views.py
@@ -10,13 +10,11 @@ class WordstemsViewSet(viewsets.ModelViewSet):
     # print('show_wordstems')
 
     def get_queryset(self):
-        print("testGetAll")
         wordstems = WordStem.objects.all()
         return wordstems
 
     @action(detail=False, methods=["get"], url_path="Str")
     def getProtoCelticStrList(self, request):
-        print("testGetPcRadicals")
         pcRadicals = WordStem.objects.all()
         pcRadicalsStr = []
         for radical in pcRadicals:
