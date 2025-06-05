4fe39646e76f00a67c67e26b45619ffec9ae80e0
Benjamin D <57868057+riwalld@users.noreply.github.com>
2025-06-05 03:20:23 +0200

    feat (api) add /api as backend root url

--- geriadur_api_django/urls.py
+++ geriadur_api_django/urls.py
@@ -1,13 +1,12 @@
-
 from . import views
 from django.urls import path, include
 
 
 urlpatterns = [
-    path('', views.home),
-    path('wordstems/', include('api_wordstems.urls')),
-    path('properNouns/', include('api_propernouns.urls')),
-    path('sessionGameData/', include('api_sessiongame.urls')),
-    path('sources/', include('api_sources.urls')),
-    path('semanticFields/', include('api_semanticfield.urls')),
+    path("api", views.home),
+    path("api/wordstems/", include("api_wordstems.urls")),
+    path("api/properNouns/", include("api_propernouns.urls")),
+    path("api/sessionGameData/", include("api_sessiongame.urls")),
+    path("api/sources/", include("api_sources.urls")),
+    path("api/semanticFields/", include("api_semanticfield.urls")),
 ]
