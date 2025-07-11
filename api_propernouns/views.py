66b61273a055ed488bd484c07561fc08633c5931
Benjamin D <57868057+riwalld@users.noreply.github.com>
2025-07-12 01:01:42 +0200

    feat (api) use APIView instead of ModelViewSet

--- api_propernouns/views.py
+++ api_propernouns/views.py
@@ -1,19 +1,24 @@
-
 from rest_framework.response import Response
 from geriadur_api_django.models import Propernoun
-from rest_framework import viewsets,status
+from rest_framework import viewsets, status
 from .serializer import PropernounSerializer
 from rest_framework.decorators import action
+from rest_framework.views import APIView
+
 
 class ProperNounsViewSet(viewsets.ModelViewSet):
     serializer_class = PropernounSerializer
-    
+
     def get_queryset(self):
         propernouns = Propernoun.objects.all()
         return propernouns
-    
-    @action(detail=False, methods=['get'], url_path='propernoun')
-    def get_one(self, request):
-        propernoun = Propernoun.objects.filter(current_name=request.query_params.get("current_name", None)).first()
-        serializer = self.get_serializer(propernoun)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class GetOneProperNounByNameView(APIView):
+    def get(self, request, name):
+        print("teststgeggoesgoien")
+        propernoun = Propernoun.objects.filter(current_name=name).first()
+        if not propernoun:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = PropernounSerializer(propernoun)
+        return Response(serializer.data)
--- geriadur_api_django/urls.py
+++ geriadur_api_django/urls.py
@@ -1,11 +1,23 @@
 from . import views
 from django.urls import path, include
+from api_propernouns.views import (
+    GetOneProperNounByNameView,
+    ProperNounsViewSet,
+)
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register(r"properNouns", ProperNounsViewSet, basename="propernouns")
 
 urlpatterns = [
-    path("api", views.home),
+    path("admin", views.home),
+    path("api/", include(router.urls)),
+    path(
+        "api/properNouns/by-name/<str:name>/",
+        GetOneProperNounByNameView.as_view(),
+        name="propernoun-by-name",
+    ),
     path("api/wordstems/", include("api_wordstems.urls")),
-    path("api/properNouns/", include("api_propernouns.urls")),
     path("api/sessionGameData/", include("api_sessiongame.urls")),
     path("api/sources/", include("api_sources.urls")),
     path("api/semanticFields/", include("api_semanticfield.urls")),
