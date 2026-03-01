671fb0d4933e70f09a16682a33481207d46b5565
Benjamin D <57868057+riwalld@users.noreply.github.com>
2026-03-01 16:27:05 +0100

    feat (settings) add static_url

--- geriadur_api_django/settings.py
+++ geriadur_api_django/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1", "gerdarzh.eu"]
 
 CSRF_TRUSTED_ORIGINS = ["https://gerdarzh.eu","http://localhost"]
 
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Application definition
 
@@ -139,11 +140,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
